eff5c2a31eda61bda3795c5a0e3981f4bc5752db
burlinchen <chenbeiliuhuo@gmail.com>
2024-10-11 16:31:30 +0800

    feat: implement repeated field support for Builder derive macro

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,10 @@
+#![feature(proc_macro_diagnostic)]
+
 use proc_macro::{token_stream, TokenStream};
 use quote::{quote, ToTokens};
+use syn::spanned::Spanned;
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let st = syn::parse_macro_input!(input as syn::DeriveInput);
 
@@ -25,8 +28,7 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
     // 下面这一行是第三关新加的
     let setter_functions = generate_setter_functions(fields)?;
     // 下面这一行是第四关新加的
-    let generated_builder_functions = generate_build_function(fields,struct_ident)?;
-
+    let generated_builder_functions = generate_build_function(fields, struct_ident)?;
 
     let ret = quote! {     // ----------------------------------+
         pub struct #builder_name_ident {                   //   |
@@ -38,7 +40,7 @@ fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
                     // 下面这一行是新加的，注意我们在这里重复展开了每一个字段
                     #(#builder_struct_factory_init_clauses),*
                 }
-                 }                                          //   |
+            }                                              //   |
         }                                                  //   |
         // 下面这三行是第三关新加的
         impl #builder_name_ident {
@@ -72,14 +74,20 @@ fn generate_builder_struct_fields_def(
 ) -> syn::Result<proc_macro2::TokenStream> {
     let idents: Vec<_> = fields.iter().map(|f| &f.ident).collect();
     // 第六关，对types 变量的构建逻辑进行了调整
-    let types: Vec<_> = fields.iter().map(|f| {
-        if let Some(inner_ty) = get_optional_inner_type(&f.ty) {
-            quote!(std::option::Option<#inner_ty>)
-        } else {
-            let origin_ty = &f.ty;
-            quote!(std::option::Option<#origin_ty>)
-        }
-    }).collect();
+    let types: Vec<_> = fields
+        .iter()
+        .map(|f| {
+            if let Some(inner_ty) = get_generic_inner_type(&f.ty, "Option") {
+                quote!(std::option::Option<#inner_ty>)
+            } else if get_user_specified_ident_for_vec(f).is_some() {
+                let origin_ty = &f.ty;
+                quote!(#origin_ty)
+            } else {
+                let origin_ty = &f.ty;
+                quote!(std::option::Option<#origin_ty>)
+            }
+        })
+        .collect();
 
     let ret = quote! {
         #(#idents: #types),*
@@ -95,8 +103,15 @@ fn generate_builder_struct_factory_init_clauses(
         .iter()
         .map(|f| {
             let ident = &f.ident;
-            quote! {
-                #ident: std::option::Option::None
+            // 下面这个if分支是第七关加入的，在第六关的时候只有else分支里的代码
+            if get_user_specified_ident_for_vec(f).is_some() {
+                quote! {
+                    #ident: std::vec::Vec::new()
+                }
+            } else {
+                quote! {
+                    #ident: std::option::Option::None
+                }
             }
         })
         .collect();
@@ -110,19 +125,44 @@ fn generate_setter_functions(fields: &StructFields) -> syn::Result<proc_macro2::
 
     let mut final_tokenstream = proc_macro2::TokenStream::new();
 
-    for (ident, ty) in idents.iter().zip(types.iter()) {
-        let tokenstream_piece;
+    // 第七关修改，这里之前用了zip串联了两个迭代器，现在需要拿到对应的原始field，所以又加了一层`enumerate()`迭代器
+    // 这里写成 for idx in 0..fields.len() {let ident = &fields[idx].ident; let type_ = &fields[idx].ty;...} 这种写法或许更优雅一些
+    for (idx, (ident, type_)) in idents.iter().zip(types.iter()).enumerate() {
+        let mut tokenstream_piece;
         // 第六关，对tokenstream_piece 变量的构建逻辑进行了调整
-        if let Some(innner_ty) = get_optional_inner_type(ty) {
+        if let Some(innner_ty) = get_generic_inner_type(type_, "Option") {
             tokenstream_piece = quote! {
                 fn #ident(&mut self, #ident: #innner_ty) -> &mut Self {
                     self.#ident = std::option::Option::Some(#ident);
                     self
                 }
             };
+        // 下面这个分支是第七关加入的
+        } else if let Some(ref user_specified_ident) =
+            get_user_specified_ident_for_vec(&fields[idx])
+        {
+            let inner_ty = get_generic_inner_type(type_, "Vec").ok_or(syn::Error::new(
+                fields[idx].span(),
+                "each field must be specified with Vec field",
+            ))?;
+            tokenstream_piece = quote! {
+                fn #user_specified_ident(&mut self, #user_specified_ident: #inner_ty) -> &mut Self {
+                    self.#ident.push(#user_specified_ident);
+                    self
+                }
+            };
+            // 如果用户指定的setter名字和原始字段的名字不一样，那么产生另一个setter，这个setter是一次性传入一个列表的
+            if user_specified_ident != ident.as_ref().unwrap() {
+                tokenstream_piece.extend(quote! {
+                    fn #ident(&mut self, #ident: #type_) -> &mut Self {
+                        self.#ident = #ident.clone();
+                        self
+                    }
+                })
+            }
         } else {
             tokenstream_piece = quote! {
-                fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                fn #ident(&mut self, #ident: #type_) -> &mut Self {
                     self.#ident = std::option::Option::Some(#ident);
                     self
                 }
@@ -135,7 +175,10 @@ fn generate_setter_functions(fields: &StructFields) -> syn::Result<proc_macro2::
     Ok(final_tokenstream)
 }
 
-fn generate_build_function(fields: &StructFields, origin_stuct_ident: &syn::Ident) -> syn::Result<proc_macro2::TokenStream> {
+fn generate_build_function(
+    fields: &StructFields,
+    origin_stuct_ident: &syn::Ident,
+) -> syn::Result<proc_macro2::TokenStream> {
     let idents: Vec<_> = fields.iter().map(|f| &f.ident).collect();
     // 下面这一行是第六关新加的，之前没用到type相关信息，就没写下面这一行
     let types: Vec<_> = fields.iter().map(|f| &f.ty).collect();
@@ -144,7 +187,10 @@ fn generate_build_function(fields: &StructFields, origin_stuct_ident: &syn::Iden
     for idx in 0..idents.len() {
         let ident = &idents[idx];
         // 第六关修改，只对不是`Option`类型的字段生成校验逻辑
-        if get_optional_inner_type(&types[idx]).is_none() {
+        // 第七关修改，只对不是`Option`类型且没有指定each属性的字段生成校验逻辑
+        if get_generic_inner_type(&types[idx], "Option").is_none()
+            && get_user_specified_ident_for_vec(&fields[idx]).is_none()
+        {
             let check_code = quote! {
                 if self.#ident.is_none() {
                     let err = format!("{} field missing", stringify!(#ident));
@@ -158,8 +204,15 @@ fn generate_build_function(fields: &StructFields, origin_stuct_ident: &syn::Iden
     let mut fill_result_clauses = Vec::new();
     for idx in 0..idents.len() {
         let ident = idents[idx];
-        // 这里需要区分`Option`类型字段和非`Option`类型字段
-        if get_optional_inner_type(&types[idx]).is_none() {
+        // 第七关，这里需要判断是否有each属性。第一个分支是本关加入的。注意这里几个分支的先后判断顺序
+        // 看我写在这里的代码可能没什么感觉，但如果是自己写的话，这几个分支的判断先后顺序是很重要的，否���可能生成出有问题的代码
+        // 这里主要的问题是梳理清楚【是否有each属性】和【是否为Option类型】这两个条件的覆盖范围
+        if get_user_specified_ident_for_vec(&fields[idx]).is_some() {
+            fill_result_clauses.push(quote! {
+                #ident: self.#ident.clone()
+            });
+        } else if get_generic_inner_type(&types[idx], "Option").is_none() {
+            // 这里需要区分`Option`类型字段和非`Option`类型字段
             fill_result_clauses.push(quote! {
                 #ident: self.#ident.clone().unwrap()
             });
@@ -181,13 +234,54 @@ fn generate_build_function(fields: &StructFields, origin_stuct_ident: &syn::Iden
     Ok(token_stream)
 }
 
-fn get_optional_inner_type(ty: &syn::Type) -> Option<&syn::Type> {
-    if let syn::Type::Path(syn::TypePath { ref path,.. }) = ty {
+fn get_generic_inner_type<'a>(ty: &'a syn::Type, outer_ident_name: &str) -> Option<&'a syn::Type> {
+    if let syn::Type::Path(syn::TypePath { ref path, .. }) = ty {
         if let Some(seg) = path.segments.last() {
-            if seg.ident == "Option" {
-                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments { ref args,.. }) = &seg.arguments {
+            if seg.ident == outer_ident_name {
+                if let syn::PathArguments::AngleBracketed(syn::AngleBracketedGenericArguments {
+                    ref args,
+                    ..
+                }) = &seg.arguments
+                {
                     if let Some(syn::GenericArgument::Type(innner_ty)) = args.first() {
-                        return Some(innner_ty)
+                        return Some(innner_ty);
+                    }
+                }
+            }
+        }
+    }
+
+    None
+}
+
+#[proc_macro_derive(ExploreAttribute)]
+pub fn attribute_explore(input: TokenStream) -> TokenStream {
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
+    let attr = st.attrs.first().unwrap();
+    let meta = attr.parse_meta(); // 解析为`sny::Meta`对象
+    eprintln!("{:#?}", meta);
+    proc_macro2::TokenStream::new().into()
+}
+
+fn get_user_specified_ident_for_vec(fields: &syn::Field) -> Option<syn::Ident> {
+    for attr in &fields.attrs {
+        if let Ok(syn::Meta::List(syn::MetaList {
+            ref path,
+            ref nested,
+            ..
+        })) = attr.parse_meta()
+        {
+            if let Some(p) = path.segments.first() {
+                if p.ident == "builder" {
+                    if let Some(syn::NestedMeta::Meta(syn::Meta::NameValue(kv))) = nested.first() {
+                        if kv.path.is_ident("each") {
+                            if let syn::Lit::Str(ref ident_str) = kv.lit {
+                                return Some(syn::Ident::new(
+                                    ident_str.value().as_str(),
+                                    attr.span(),
+                                ));
+                            }
+                        }
                     }
                 }
             }
