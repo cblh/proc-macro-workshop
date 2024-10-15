964f9d8b3d0914e9f589e35f46d91f57abcb624f
burlinchen <chenbeiliuhuo@gmail.com>
2024-10-15 17:19:13 +0800

    feat: Implement basic structure for CustomDebug derive macro

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use syn::{self, spanned::Spanned};
+use quote::{ToTokens, quote};
 
 #[proc_macro_derive(CustomDebug)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
+    match do_expand(&st) {
+        Ok(t) => t.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
 
-    unimplemented!()
+fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let ret = proc_macro2::TokenStream::new();
+    return Ok(ret)
 }
