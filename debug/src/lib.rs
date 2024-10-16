b4a23e2ede2ddfe4bb62d2691fd4ba3ac21c8f48
burlinchen <chenbeiliuhuo@gmail.com>
2024-10-16 11:11:32 +0800

    feat: implement Debug trait derivation for structs

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -12,6 +12,55 @@ pub fn derive(input: TokenStream) -> TokenStream {
 }
 
 fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
-    let ret = proc_macro2::TokenStream::new();
+    let ret = generate_debug_trait(st)?;
     return Ok(ret)
+}
+
+type StructFields = syn::punctuated::Punctuated<syn::Field, syn::Token!(,)>;
+fn get_fields_from_derive_input(d: &syn::DeriveInput) -> syn::Result<&StructFields> {
+    match &d.data {
+        syn::Data::Struct(syn::DataStruct {
+            fields: syn::Fields::Named(syn::FieldsNamed { ref named,.. }),
+           ..
+        }) => Ok(named),
+        _ => Err(syn::Error::new_spanned(
+            d,
+            "Must define on a Struct, not Enum".to_string(),
+        )),
+    }
+}
+
+fn generate_debug_trait(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let fields = get_fields_from_derive_input(st)?;
+    let struct_name_ident = &st.ident;
+    let struct_name_literal = struct_name_ident.to_string();
+
+    let mut fmt_body_stream = proc_macro2::TokenStream::new();
+
+    fmt_body_stream.extend(quote!(
+        fmt.debug_struct(#struct_name_literal)
+    ));
+
+    for field in fields.iter() {
+        let field_name_idnet = field.ident.as_ref().unwrap();
+        let field_name_literal = field_name_idnet.to_string();
+
+        fmt_body_stream.extend(quote!(
+           .field(#field_name_literal, &self.#field_name_idnet)
+        ));
+    }
+
+    fmt_body_stream.extend(quote!(
+       .finish()
+    ));
+
+    let ret_stream = quote!(
+        impl std::fmt::Debug for #struct_name_ident {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                #fmt_body_stream
+            }
+        }
+    );
+
+    Ok(ret_stream)
 }
